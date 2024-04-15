1ed70ef382e7ba91a3f132c31ad7d98b970de885
WangariNelly <nellywangari30@gmail.com>
2024-04-15 15:40:06 +0300

    Sum Functions

--- Day1/__tests__/day1.spec.js
+++ Day1/__tests__/day1.spec.js
@@ -1,4 +1,4 @@
-const {dayOneSol1,dayOneSol2} = require("../day1.js");
+const {dayOneSol1,dayOneSol2,sumAll} = require("../day1.js");
 
 describe("test dayOneSol1 function", () => {
     it("returns 2", () => {
@@ -9,5 +9,11 @@ describe("test dayOneSol1 function", () => {
 describe("test dayOneSol2 function", () => {
     it("returns 3", () => {
         expect(dayOneSol2()).toBe(3)
+    })
+})
+
+describe("test sumAll function", () => {
+    it("returns 3", () => {
+        expect(sumAll(1,2)).toBe(3)
     })
 })
